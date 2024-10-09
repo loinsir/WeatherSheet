319063ca626041efaf76d66ac6691684180b86c1
Inhwan Kim <a9327370@gmail.com>
2024-10-10 00:50:31 +0900

    Initial Commit

---
+++ WeatherSheet/WeatherSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSheetApp.swift
+//  WeatherSheet
+//
+//  Created by 김인환 on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherSheetTests/WeatherSheetTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSheetTests.swift
+//  WeatherSheetTests
+//
+//  Created by 김인환 on 10/10/24.
+//
+
+import Testing
+@testable import WeatherSheet
+
+struct WeatherSheetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
